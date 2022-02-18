c3d1c6e285b80e678fdac113422c7b9a48501028
Rehan Shekh <rehanrshekh@gmail.com>
2022-02-18 11:16:16 +0530

    Add java programs

---
+++ com/blz/Addition.java
@@ -0,0 +1,10 @@
+package com.blz;
+
+public class Addition {
+    public static void main(String[] args) {
+        int x = 33, y = 55, a = -15, b = -23, sum, s;
+        sum = (x - y);
+        System.out.println("The sum of x and y is:"+sum);
+
+    }
+}
